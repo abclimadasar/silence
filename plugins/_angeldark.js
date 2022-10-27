19db2ccf3d7d6244e0eaacd4fc6a6fe2c1c5d2a2
Abdul Mufid <110360966+eabdalmufid@users.noreply.github.com>
2022-10-27 08:03:30 +0700

    New

--- plugins/_angeldark.js
+++ plugins/_angeldark.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
 await m.reply(global.wait)
   conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/kanna?apikey=ed78c137a46873c5b8e5fe3b', '', '', m)
 }
-handler.help = ['dave']
+handler.help = ['angeldark']
 handler.tags = ['anime']
-handler.command = /^(dave)$/i
+handler.command = /^(angeldark)$/i
 handler.limit = true
 
 export default handler 
